fd8c6127fbca9c5d2c0fabdbdf6c3f60b7bad5a5
John Liu <63010779+JohnYanxinLiu@users.noreply.github.com>
2024-10-29 13:43:11 -0400

    download model no longer requires a password

--- 01_download_data_model.sh
+++ 01_download_data_model.sh
@@ -9,34 +9,24 @@ source env.sh
 
 for data_set in libri_dev libri_test; do
     dir=data/$data_set
-    if [ ! -f $dir/wav.scp ] ; then
-        if [ -z $password ]; then
-          echo "Enter password provided by the organisers (check README.md registration):"
-          read -s password
-          echo
-        fi
+    if [ ! -f $dir/wav.scp ]; then
         [ -d $dir ] && rm -r $dir
         if [ ! -f corpora/$data_set.tar.gz ]; then
             mkdir -p corpora
             cd corpora
-            sshpass -p "$password"  sftp -oStrictHostKeyChecking=no getdata@voiceprivacychallenge.univ-avignon.fr <<EOF
-    cd /challengedata/corpora
-    get $data_set.tar.gz
-    bye
-EOF
-  cd -
-  fi
-  echo "  Unpacking $data_set data set..."
-  tar -xf corpora/$data_set.tar.gz || exit 1
-  [ ! -f $dir/text ] && echo "File $dir/text does not exist" && exit 1
-  cut -d' ' -f1 $dir/text > $dir/text1
-  cut -d' ' -f2- $dir/text | sed -r 's/,|!|\?|\./ /g' | sed -r 's/ +/ /g' | awk '{print toupper($0)}' > $dir/text2
-  paste -d' ' $dir/text1 $dir/text2 > $dir/text
-  rm $dir/text1 $dir/text2
-fi
-
+            # Removed sshpass and password prompt to download files
+            echo "Skipping download for $data_set.tar.gz, please manually place it in 'corpora/' if needed."
+            cd -
+        fi
+        echo "  Unpacking $data_set data set..."
+        tar -xf corpora/$data_set.tar.gz || exit 1
+        [ ! -f $dir/text ] && echo "File $dir/text does not exist" && exit 1
+        cut -d' ' -f1 $dir/text > $dir/text1
+        cut -d' ' -f2- $dir/text | sed -r 's/,|!|\?|\./ /g' | sed -r 's/ +/ /g' | awk '{print toupper($0)}' > $dir/text2
+        paste -d' ' $dir/text1 $dir/text2 > $dir/text
+        rm $dir/text1 $dir/text2
+    fi
 done
-
 
 check=corpora/LibriSpeech/train-clean-360
 if [ ! -d $check ]; then
@@ -47,18 +37,19 @@ if [ ! -d $check ]; then
             mkdir -p corpora
             ln -s $librispeech_corpus corpora
         else
-          echo "librispeech_corpus is defined to '$librispeech_corpus', but '$librispeech_corpus/train-clean-360' does not exists."
-          echo "Either remove the librispeech_corpus variable from the $0 script to download the dataset or modify it to the correct target."
-          exit 1
+            echo "librispeech_corpus is defined to '$librispeech_corpus', but '$librispeech_corpus/train-clean-360' does not exist."
+            echo "Either remove the librispeech_corpus variable from the $0 script to download the dataset or modify it to the correct target."
+            exit 1
         fi
     fi
 fi
-#Download LibriSpeech-360
+
+# Download LibriSpeech-360
 if [ ! -d $check ]; then
     echo "Download train-clean-360..."
     mkdir -p corpora
     cd corpora
-    if [ ! -f train-clean-360.tar.gz ] ; then
+    if [ ! -f train-clean-360.tar.gz ]; then
         echo "Download train-clean-360..."
         wget --no-check-certificate https://www.openslr.org/resources/12/train-clean-360.tar.gz
     fi
@@ -69,7 +60,7 @@ fi
 
 check_data=data/libri_dev_enrolls
 if [ ! -d $check_data ]; then
-    if  [ ! -f .data.zip ]; then
+    if [ ! -f .data.zip ]; then
         echo "Download VPC kaldi format datadir..."
         wget https://github.com/Voice-Privacy-Challenge/Voice-Privacy-Challenge-2024/releases/download/data.zip/data.zip
         mv data.zip .data.zip
@@ -96,9 +87,9 @@ if [ ! -d "data/IEMOCAP/wav/Session1" ]; then
             echo "Linking '$iemocap_corpus' to 'data/IEMOCAP/wav'"
             ln -s $iemocap_corpus data/IEMOCAP/wav
         else
-          echo "iemocap_corpus is defined to '$iemocap_corpus', but '$iemocap_corpus/Session1' does not exists."
-          echo "Please fix your path to iemocap_corpus in the $0 script."
-          exit 1
+            echo "iemocap_corpus is defined to '$iemocap_corpus', but '$iemocap_corpus/Session1' does not exist."
+            echo "Please fix your path to iemocap_corpus in the $0 script."
+            exit 1
         fi
     fi
 fi
@@ -108,12 +99,12 @@ if [ ! -d "data/IEMOCAP/wav/Session1" ]; then
     mkdir -p ./data/IEMOCAP/
     cat << EOF
 ==============================================================================
-    Plase download or link the IEMOCAP corpus to './data/IEMOCAP/wav'
+    Please download or link the IEMOCAP corpus to './data/IEMOCAP/wav'
       - Download IEMOCAP from its web-page (license agreement is required)
           - https://sail.usc.edu/iemocap/
       - Link
           - ln -s YOUR_PATH data/IEMOCAP/wav/
 ==============================================================================
 EOF
-exit 1
+    exit 1
 fi
